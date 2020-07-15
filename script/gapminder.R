f1f0236308f49a705262343e300ea8e1603a693c
mohrosidi <moh.rosidi2610@gmail.com>
2020-07-15 12:59:19 +0000

    menghapus file tidak penting dari cabang answer

--- script/gapminder.R
+++
@@ -1,9 +0,0 @@
-# 1. Load Library ----
-if(!require(tidyverse)) install.packages("tidyverse")
-if(!require(gapminder)) install.packages("gapminder")
-
-library(tidyverse)
-library(gapminder)
-
-# 2. Save Clean Data -----
-write_csv(gapminder, "data/gapminder.csv")
